b9d3c42376dfb0d4c22978d7892f0451ad52561e
uysolanki <uysolanki@gmail.com>
2026-07-22 16:08:54 +0530

    create table

--- src/JdbcDriverApp.java
+++ src/JdbcDriverApp.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class JdbcDriverApp {
 
@@ -10,7 +11,19 @@ public class JdbcDriverApp {
 		    "jdbc:mysql://localhost:3306/upgradb11jdbcdb", "root", ""
 		);
 		
-		System.out.println(connection.isClosed()); //false
+		//System.out.println(connection.isClosed()); //false
+		
+		String query="create table employeeUpgrad(\r\n"								//static
+				+ "eno int primary key  auto_increment,\r\n"
+				+ "ename varchar(15) not null,\r\n"
+				+ "eage int,\r\n"
+				+ "egen enum('male','female'))";
+		
+		Statement st=connection.createStatement();
+		
+		st.execute(query);
+		
+		System.out.println("Table created");
 	}
 
 }
